dde98810192a861ffe4785beb3433e59e365bc68
Ben Reynolds <ben.reynolds@drizly.com>
2021-08-17 09:27:19 -0400

    Try again

--- .github/workflow-scripts/check-version-available.sh
+++ .github/workflow-scripts/check-version-available.sh
@@ -2,4 +2,4 @@
 
 VERSION=$1
 
-curl  ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/git/matching-refs/tags | jq -e --arg VERSION "$VERSION" '.[] | select(.ref | endswith($VERSION))'
+! curl -s ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/git/matching-refs/tags | jq -e --arg VERSION "$VERSION" '.[] | select(.ref | endswith($VERSION))'
